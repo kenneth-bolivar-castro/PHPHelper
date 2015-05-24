46bf0d6a6e7b070e3832d8c0be777a2e8a3bd4cb
Kenneth Bolivar <kenneth@anexusit.com>
2015-05-23 23:29:25 -0600

    Added home page

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @filesource
+ * Home page to PHPHelper.
+ */
+?>
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+  <head>
+    <title>PHPHelper</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+  </head>
+  <body>
+    <h1>PHPHelper Project</h1>
+    <div>Classes and functions helpers to develop on PHP projects.</div>
+  </body>
+</html>
